9c9b3953c9775145c880640fb6960bdf3d15b7d8
Ryan <81394855+cloudthepain@users.noreply.github.com>
2023-11-06 14:28:35 -0500

    Added PlayerStats, Jump & Adjusted the Movement
    Player Status must be initialized OnNetworkSpanw.

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerStats : MonoBehaviour
+public class PlayerStats : NetworkBehaviour
 {
-    int PlayerHealth;
-    int PlayerMana;
+    public NetworkVariable<float> playerHealth;
+	public NetworkVariable<float> playerMana;
+	public NetworkVariable<float> playerAttack;
+	public NetworkVariable<float> playerSpeed;
+	public NetworkVariable<float> jumpPower;
 
-
+	public override void OnNetworkSpawn()
+	{
+		base.OnNetworkSpawn();
+		playerHealth.Value = 0;
+		playerMana.Value = 0;
+		playerAttack.Value = 0;
+		playerSpeed.Value = 20;
+		jumpPower.Value = 20;
+	}
 }
